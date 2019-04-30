373b353d29c8e41efb1840e7db50f5e5257f45f0
up201806469 <dami@outlook.pt>
2019-04-30 08:45:02 +0100

    dia por acabar

---
+++ Trabalho PC II/dia.vb
@@ -0,0 +1,17 @@
+﻿Public Class dia
+    Private _estado As Integer
+
+    Public Property Estado As Integer
+        Get
+            Return _estado
+        End Get
+        Set(value As Integer)
+            _estado = value
+        End Set
+    End Property
+
+    Public Sub New()
+
+    End Sub
+    Public Sub New(ByVal estado)
+End Class
